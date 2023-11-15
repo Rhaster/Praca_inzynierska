fdf4f3392802ef2a74297dbd09515e92d434e2ba
Rhaster <73538677+Rhaster@users.noreply.github.com>
2023-11-15 04:32:23 +0100

    UI_Main_menu_muzyka+efekty
    Implementacja ustawien do dzwieku w menu głównym

--- Assets/Scenes/MainMenu/SkryptyMenu/MusicManager.cs
+++ Assets/Scenes/MainMenu/SkryptyMenu/MusicManager.cs
@@ -1,42 +1,86 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 public class MusicManager : MonoBehaviour
 {
-    public Slider volumeSlider; 
-    public AudioSource musicAudioSource; 
 
+    public static MusicManager Instance { get; private set; }
+
+    public Slider volumeMusicSlider;
+    public Slider volumeSoundSlider;
+    public AudioMixerGroup musicAudioGroup;
+    public AudioMixerGroup soundEffectsGroup;
     public Color lowVolumeColor = Color.green;
     public Color mediumVolumeColor = Color.yellow;
     public Color highVolumeColor = Color.red;
+    //const string MIXER_MUSIC = "MusicVolume";
+    //const string MIXER_SOUND = "SOUNDVolume";
     private Image sliderFill;
+    private Image sliderSoundFill;
+
+    [SerializeField] private float soundVolume;
+    [SerializeField] private float musicVolume;
+
+    private void Awake()
+    {
+        Instance = this;
+        volumeSoundSlider.value = 1;
+        volumeMusicSlider.value = 1;
+    }
+    
 
     void Start()
     {
-        // Ustaw wartoœæ suwaka na pocz¹tkow¹ g³oœnoœæ
-        volumeSlider.value = musicAudioSource.volume;
+        volumeSoundSlider.value = soundVolume;
+        volumeSoundSlider.onValueChanged.AddListener(SetSoundVolume);
+        sliderFill = volumeSoundSlider.fillRect.GetComponent<Image>();
 
-        // Dodaj obs³ugê zdarzenia na zmianê wartoœci suwaka
-        volumeSlider.onValueChanged.AddListener(ChangeVolume);
-        sliderFill = volumeSlider.fillRect.GetComponent<Image>();
+        volumeMusicSlider.value = musicVolume;
+        volumeMusicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sliderSoundFill = volumeMusicSlider.fillRect.GetComponent<Image>();
+        sliderFill.color = highVolumeColor;
+        sliderSoundFill.color = highVolumeColor;
     }
 
-    void ChangeVolume(float volume)
+    private void SetMusicVolume(float volume)
     {
-        // Ustaw g³oœnoœæ Audio Source na podstawie wartoœci suwaka (0.0 do 1.0)
-        musicAudioSource.volume = volume;
-        ChangeColorByVolume(volume);
+        volume = Mathf.Clamp(volume, (float)0.001, 1);
+        musicAudioGroup.audioMixer.SetFloat("Volume1", Mathf.Log10(volume) * 20);
+        musicVolume= volume;
+        ChangeColorByVolume1(volume, volumeMusicSlider);
     }
-    void ChangeColorByVolume(float volume)
+    private void SetSoundVolume(float volume)
     {
-        // Przelicz wartoœæ suwaka na zakres 0-1
-        float normalizedVolume = Mathf.InverseLerp(volumeSlider.minValue, volumeSlider.maxValue, volume);
+        volume = Mathf.Clamp(volume, (float)0.001, 1);
+        soundEffectsGroup.audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+        soundVolume = volume;
+        ChangeColorByVolume(volume, volumeSoundSlider);
+    }
 
-        // Interpoluj kolor na podstawie gradientu
+    void ChangeColorByVolume(float volume1, Slider x)
+    {
+        float normalizedVolume = Mathf.InverseLerp(x.minValue, x.maxValue, volume1);
         Color newColor = Color.Lerp(lowVolumeColor, highVolumeColor, normalizedVolume);
-
-        // Ustaw nowy kolor w komponencie Image suwaka
         sliderFill.color = newColor;
     }
+    void ChangeColorByVolume1(float volume, Slider x)
+    {
+        float normalizedVolume = Mathf.InverseLerp(x.minValue, x.maxValue, volume);
+        Color newColor = Color.Lerp(lowVolumeColor, highVolumeColor, normalizedVolume);
+        sliderSoundFill.color = newColor;
+    }
+    public float GetSoundVolume()
+    {
+        return 1;//return SoundVolume;
+    }
+    public float GetMusicVolume()
+    {
+        return 1;//MusicVolume;
+    }
 }
+ 
